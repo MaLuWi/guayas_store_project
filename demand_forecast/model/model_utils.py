4f388a13a117e5f14d0e725203e9b0f7b4e540f4
MaLuWi <maluwi.podadera@gmx.de>
2025-05-19 11:49:13 +0200

    Update model_utils.py

--- demand_forecast/model/model_utils.py
+++ demand_forecast/model/model_utils.py
@@ -3,8 +3,6 @@ import xgboost as xgb
 import pandas as pd
 from data.data_utils import generate_future_data
 from app.config import MODEL_PATH
-from sklearn.preprocessing import LabelEncoder
-
 
 def load_model(model_path=MODEL_PATH):
     model_file = os.path.join(model_path, "model.xgb")
@@ -15,7 +13,7 @@ def load_model(model_path=MODEL_PATH):
     return model
 
 def predict(model, input_data):
-    drop_cols = [c for c in ('date','unit_sales') if c in input_data]
+    drop_cols = [c for c in ('date', 'unit_sales') if c in input_data]
     return model.predict(input_data.drop(columns=drop_cols))
 
 def forecast_timeseries(model, store_id, item_id, start_date, horizon, df_train, df_stores, df_items):
@@ -23,10 +21,10 @@ def forecast_timeseries(model, store_id, item_id, start_date, horizon, df_train,
     if future_df.empty:
         return pd.DataFrame()
     final_feature_list = [
-        'store_nbr','item_nbr','onpromotion','year','month','day','day_of_week',
-        'unit_sales_7d_avg','lag_1','lag_7','lag_14','lag_30',
-        'roll7_std','pct_chg_7d','is_weekend',
-        'city','state','cluster','family','class','perishable'
+        'store_nbr', 'item_nbr', 'onpromotion', 'year', 'month', 'day', 'day_of_week',
+        'unit_sales_7d_avg', 'lag_1', 'lag_7', 'lag_14', 'lag_30',
+        'roll7_std', 'pct_chg_7d', 'is_weekend',
+        'city', 'state', 'cluster', 'family', 'class', 'perishable'
     ]
     features = [c for c in final_feature_list if c in future_df.columns]
     preds = model.predict(future_df[features])
