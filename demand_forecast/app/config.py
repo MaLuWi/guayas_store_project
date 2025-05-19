c36733cf740fe2eecd3e554c1b89c9a82ff8e66d
MaLuWi <maluwi.podadera@gmx.de>
2025-05-19 12:26:38 +0200

    Update config.py

--- demand_forecast/app/config.py
+++ demand_forecast/app/config.py
@@ -5,18 +5,12 @@ DATA_PATH = "data/"
 # Google Drive file IDs for each dataset
 your_file_id_for_stores_csv = '1OUyap9CGF59ewdkN3DM6NWw3nv-hT5pH'
 your_file_id_for_items_csv = '1KPu1RTEQQCA_GoNEapFtq8oZTPmyvMFx'
-your_file_id_for_transactions_csv = '1FSrI_JqbzSmZ0c4NBmfiMT-kYQG31xUS'
-your_file_id_for_oil_csv = '15uMxMXXSYEv14Yv4cLyKuiCEuO1TmXrU'
-your_file_id_for_holidays_csv = '1fNj8v8kDmfdslU_M9FgmmV_FPqd1tIN1'
-your_file_id_for_train_csv = '1KsbtNzrfL5ZTFLpnaCyEUAmRzA2EBDLz'
+your_file_id_for_train_csv = '14cGjnrhqQaBZV9BZmJpO0qGftMENKOAM'
 
 # Google Drive download links for each file (use these for gdown or programmatic download!)
 GOOGLE_DRIVE_LINKS = {
     "stores": f"https://drive.google.com/uc?id={your_file_id_for_stores_csv}",
     "items": f"https://drive.google.com/uc?id={your_file_id_for_items_csv}",
-    "transactions": f"https://drive.google.com/uc?id={your_file_id_for_transactions_csv}",
-    "oil": f"https://drive.google.com/uc?id={your_file_id_for_oil_csv}",
-    "holidays_events": f"https://drive.google.com/uc?id={your_file_id_for_holidays_csv}",
     "train": f"https://drive.google.com/uc?id={your_file_id_for_train_csv}"
 }
 
